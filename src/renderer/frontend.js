7280c48065750407782eb0b88ca1cef51fe89345
Adrian Estrada <edsadr@gmail.com>
2017-05-09 13:22:05 -0500

    Buscar imágenes

--- src/renderer/frontend.js
+++ src/renderer/frontend.js
@@ -1,6 +1,9 @@
+import url from 'url'
+import path from 'path'
 
 window.addEventListener('load', () => {
   addImagesEvents()
+  searImagesEvent()
 })
 
 function addImagesEvents () {
@@ -17,4 +20,32 @@ function changeImage (node) {
   document.querySelector('li.selected').classList.remove('selected')
   node.classList.add('selected')
   document.getElementById('image-displayed').src = node.querySelector('img').src
+}
+
+function searImagesEvent () {
+  const searchBox = document.getElementById('search-box')
+
+  searchBox.addEventListener('keyup', function () {
+    const regex = new RegExp(this.value.toLowerCase(), 'gi')
+
+    if (this.value.length > 0) {
+      const thumbs = document.querySelectorAll('li.list-group-item img')
+      for (let i = 0, length1 = thumbs.length; i < length1; i++) {
+        const fileUrl = url.parse(thumbs[i].src)
+        const fileName = path.basename(fileUrl.pathname)
+        if (fileName.match(regex)) {
+          thumbs[i].parentNode.classList.remove('hidden')
+        } else {
+          thumbs[i].parentNode.classList.add('hidden')
+        }
+      }
+
+      selectFisrtImage()
+    }
+  })
+}
+
+function selectFisrtImage () {
+  const image = document.querySelector('li.list-group-item:not(.hidden)')
+  changeImage(image)
 }
