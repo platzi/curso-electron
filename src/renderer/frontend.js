77b3a98d2a65fcf50b0e106389bb765971eca942
Adrian Estrada <edsadr@gmail.com>
2017-05-09 13:03:56 -0500

    Selecciona imágenes

--- src/renderer/frontend.js
+++ src/renderer/frontend.js
@@ -1,5 +1,20 @@
-import os from 'os'
 
 window.addEventListener('load', () => {
-  console.log(os.cpus())
+  addImagesEvents()
 })
+
+function addImagesEvents () {
+  const thumbs = document.querySelectorAll('li.list-group-item')
+
+  for (let i = 0, length1 = thumbs.length; i < length1; i++) {
+    thumbs[i].addEventListener('click', function () {
+      changeImage(this)
+    })
+  }
+}
+
+function changeImage (node) {
+  document.querySelector('li.selected').classList.remove('selected')
+  node.classList.add('selected')
+  document.getElementById('image-displayed').src = node.querySelector('img').src
+}
