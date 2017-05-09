f3c5b072a8a4c3fa3a00992a0176a9fcdab0532f
Adrian Estrada <edsadr@gmail.com>
2017-05-09 10:51:57 -0500

    Herramientas de desarrollo para Electron

--- src/devtools.js
+++ src/devtools.js
@@ -1,6 +1,9 @@
 
 import { enableLiveReload } from 'electron-compile'
+import electronDebug from 'electron-debug'
 
 module.exports = function devtools () {
   enableLiveReload()
+  // Mostrando las herramientas de DevTools para las diferentes ventanas
+  electronDebug({ showDevTools: true })
 }
